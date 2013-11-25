9a5004e251ba0306eb3933f103c982848afd3888
manne ohrstrom <manne@shotgunsoftware.com>
2013-11-25 20:41:12 +0000

    overlay widget now works nicely.

--- python/tk_multi_loader/overlaywidget.py
+++ python/tk_multi_loader/overlaywidget.py
@@ -8,9 +8,6 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import os
-import sys
-import threading
 
 from tank.platform.qt import QtCore, QtGui
 
@@ -20,7 +17,6 @@ class ResizeEventFilter(QtCore.QObject):
     Event filter which emits a resized signal whenever
     the monitored widget resizes
     """
-    
     resized = QtCore.Signal()
 
     def eventFilter(self,  obj,  event):
@@ -37,16 +33,24 @@ class OverlayWidget(QtGui.QWidget):
     Overlay widget that can be placed on top over any other widget.
     """
     
+    MODE_OFF = 0
+    MODE_SPIN = 1
+    MODE_ERROR = 2
+    MODE_INFO_TEXT = 3
+    MODE_INFO_PIXMAP = 4
+    
+    
     def __init__(self, parent=None):
         
         QtGui.QWidget.__init__(self, parent)
         
         self._parent = parent
         
+        # hook up a listener to the parent window so we 
+        # can resize the overlay at the same time as the parent window
+        # is being resized.
         filter = ResizeEventFilter(self._parent)
-        
         filter.resized.connect(self._on_parent_resized)
-        
         self._parent.installEventFilter(filter)
         
         # make it transparent
@@ -54,22 +58,24 @@ class OverlayWidget(QtGui.QWidget):
         
         # turn off the widget
         self.setVisible(False)
+        self._mode = OverlayWidget.MODE_OFF
         
-        # create spinner timer
+        # setup spinner timer
         self._timer = QtCore.QTimer(self)
         self._timer.timeout.connect(self.on_animation)
         self._spin_angle = 0
         
-        self._spin = False
-        self._message = ""
+        self._message_pixmap = None
+        self._message = None
         self._sg_icon = QtGui.QPixmap(":/res/sg_icon.png")
  
     def _on_parent_resized(self):
         """
-        When parent is resized
+        When parent is resized, resize the overlay
         """
         self.resize(self._parent.size())
  
+    
     def on_animation(self):
         """
         Spinner callback
@@ -79,54 +85,27 @@ class OverlayWidget(QtGui.QWidget):
             self._spin_angle = 0
         self.repaint()
  
-    def start_spin(self):
-        """
-        Spin
-        """
-        self._timer.start(40)
-        self.setVisible(True)
-        self._spin = True
-
-    def _ensure_not_spinning(self):
-        self._spin = False
-        self._timer.stop()
-    
-    def show_error_message(self, msg):
-        """
-        Error
-        """
-        self._ensure_not_spinning()
-        self.setVisible(True)
-        self._message = "ERROR: %s" % msg
- 
-    def show_message(self, msg):
-        """
-        Error
-        """
-        self._ensure_not_spinning()
-        self.setVisible(True)
-        self._message = msg
-
-    def hide(self):
-        self._ensure_not_spinning()
-        self.setVisible(False)
- 
     def paintEvent(self, event):
         """
         Render the UI
         """
+        if self._mode == OverlayWidget.MODE_OFF:
+            return
+        
         painter = QtGui.QPainter()
         painter.begin(self)
         try:
-            
+            # set up semi transparent backdrop
             painter.setRenderHint(QtGui.QPainter.Antialiasing)
             overlay_color = QtGui.QColor(0,0,0,140)
             painter.setBrush( QtGui.QBrush(overlay_color))
             painter.setPen(QtGui.QPen(overlay_color))
             painter.drawRect( 0, 0, painter.device().width(), painter.device().height())
 
-            if self._spin:
-            
+            # now draw different things depending on mode
+            if self._mode == OverlayWidget.MODE_SPIN:
+                # show the spinner
+                
                 painter.translate((painter.device().width() / 2) - 40, 
                                   (painter.device().height() / 2) - 40)
                 
@@ -141,22 +120,83 @@ class OverlayWidget(QtGui.QWidget):
     
                 painter.drawArc(r, start_angle, span_angle)
             
-            else:
-                # draw message
+            elif self._mode == OverlayWidget.MODE_INFO_TEXT:
+                # show text in the middle
+                pen = QtGui.QPen(QtGui.QColor("#888888"))
+                painter.setPen(pen)            
+                text_rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
+                text_flags = QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter | QtCore.Qt.TextWordWrap
+                painter.drawText(text_rect, text_flags, self._message)            
                 
-                pen = QtGui.QPen(QtGui.QColor("#424141"))
-                pen.setWidth(3)
-                painter.setPen(pen)
-            
-                painter.translate((painter.device().width() / 2) - 40, 
-                                  (painter.device().height() / 2) - 40)
-            
-                r = QtCore.QRect(0, 0, 80, 80)
-                painter.drawText(r, QtCore.Qt.AlignCenter, self._message);            
+            elif self._mode == OverlayWidget.MODE_ERROR:
+                # show error text in the center
+                pen = QtGui.QPen(QtGui.QColor("#C8534A"))
+                painter.setPen(pen)            
+                msg = "An error occurred: %s" % self._message
+                text_rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
+                text_flags = QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter | QtCore.Qt.TextWordWrap
+                painter.drawText(text_rect, text_flags, msg)            
+
+            elif self._mode == OverlayWidget.MODE_INFO_PIXMAP:
+                # draw image
+                painter.translate((painter.device().width() / 2) - (self._message_pixmap.width()/2), 
+                                  (painter.device().height() / 2) - (self._message_pixmap.height()/2) )
+                
+                painter.drawPixmap( QtCore.QPoint(0, 0), self._message_pixmap)
+                
             
         finally:
             painter.end()
         
         
         
+    ############################################################################################
+    # public interface
+    
+    def start_spin(self):
+        """
+        Turn on spinning
+        """
+        self._timer.start(40)
+        self.setVisible(True)
+        self._mode = OverlayWidget.MODE_SPIN
+
+    def show_error_message(self, msg):
+        """
+        Display an error message
+        """
+        self._timer.stop()
+        self.setVisible(True)
+        self._message = msg
+        self._mode = OverlayWidget.MODE_ERROR
+        self.repaint()
+ 
+    def show_message(self, msg):
+        """
+        Show an info message
+        """
+        self._timer.stop()
+        self.setVisible(True)
+        self._message = msg
+        self._mode = OverlayWidget.MODE_INFO_TEXT
+        self.repaint()
+        
+    def show_message_pixmap(self, pixmap):
+        """
+        Show an info message in the form of a pixmap
+        """
+        self._timer.stop()
+        self.setVisible(True)
+        self._message_pixmap = pixmap
+        self._mode = OverlayWidget.MODE_INFO_PIXMAP
+        self.repaint()
+
+    def hide(self):
+        """
+        Hide the overlay
+        """
+        self._timer.stop()
+        self._mode = OverlayWidget.MODE_OFF
+        self.setVisible(False)
+ 
         

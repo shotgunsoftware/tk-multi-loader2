0122e776afeab2f419d52bcccabc43b92047d77b
manne ohrstrom <manne@shotgunsoftware.com>
2014-03-18 10:33:44 +0000

    Added message informing users that they are running an alpha version.

--- python/tk_multi_loader/__init__.py
+++ python/tk_multi_loader/__init__.py
@@ -8,9 +8,21 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+from sgtk.platform.qt import QtCore, QtGui
+
 def show_dialog(app):
     # defer imports so that the app works gracefully in batch modes
     from .dialog import AppDialog
+    
+    # alpha message
+    msg =  "Welcome to the Toolkit Loader Evaluation!\n\n"
+    msg += "This is an alpha version for evaluation only.\n"
+    msg += "Note that there may be bugs and missing features.\n\n"
+    msg += "Please send feedback to toolkitsupport@shotgunsoftware.com"
+    
+    QtGui.QMessageBox.information(None, "Toolkit Loader Evaluation", msg)
+    
+    # start ui
     ui_title = app.get_setting("title_name")
     app.engine.show_dialog(ui_title, app, AppDialog)
     

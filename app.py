ef11767aa9f84a4594858e7c0874bf01c1615ce4
manne ohrstrom <manne@shotgunsoftware.com>
2014-04-10 14:39:47 +0100

    Added short name command

--- app.py
+++ app.py
@@ -26,10 +26,11 @@ class MultiLoader(sgtk.platform.Application):
         """
         
         tk_multi_loader = self.import_module("tk_multi_loader")
-        cb = lambda : tk_multi_loader.show_dialog(self)
-        menu_caption = self.get_setting("menu_name")
         
-        # add stuff to main menu
-        self.engine.register_command(menu_caption, cb)        
+        # register command
+        cb = lambda : tk_multi_loader.show_dialog(self)
+        menu_caption = "%s..." % self.get_setting("menu_name")
+        menu_options = { "short_name": self.get_setting("menu_name").replace(" ", "_") }
+        self.engine.register_command(menu_caption, cb, menu_options)        
         
 

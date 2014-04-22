780b2aca7e6d6d7f4207c6460eb44aaf17b73606
manne ohrstrom <manne@shotgunsoftware.com>
2014-04-22 15:37:45 +0100

    Added standard shotgun icons to tree view.

--- python/tk_multi_loader/model_entity.py
+++ python/tk_multi_loader/model_entity.py
@@ -27,7 +27,22 @@ class SgEntityModel(ShotgunOverlayModel):
         Constructor
         """
         # folder icon
-        self._folder_icon = QtGui.QIcon(QtGui.QPixmap(":/res/folder_512x400.png"))    
+        self._default_icon = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Folder.png"))    
+        
+        # shotgun entity icons
+        self._entity_icons = {}
+        self._entity_icons["Shot"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Shot_dark.png"))
+        self._entity_icons["Asset"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Asset_dark.png"))
+        self._entity_icons["EventLogEntry"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_EventLogEntry_dark.png"))
+        self._entity_icons["Group"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Group_dark.png"))
+        self._entity_icons["HumanUser"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_HumanUser_dark.png"))
+        self._entity_icons["Note"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Note_dark.png"))
+        self._entity_icons["Project"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Project_dark.png"))
+        self._entity_icons["Sequence"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Sequence_dark.png"))
+        self._entity_icons["Task"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Task_dark.png"))
+        self._entity_icons["Ticket"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Ticket_dark.png"))
+        self._entity_icons["Version"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Version_dark.png"))
+        
         ShotgunOverlayModel.__init__(self, 
                                      parent, 
                                      overlay_widget, 
@@ -59,11 +74,33 @@ class SgEntityModel(ShotgunOverlayModel):
         :param sg_data: Shotgun data dictionary that was received from Shotgun given the fields
                         and other settings specified in load_data()
         """
+        found_icon = False
         
-        field_data = shotgun_model.get_sg_data(item)        
-        if field_data is None:
-            # intermediate node
-            item.setIcon(self._folder_icon)
+        # get the associated field data with this node
+        field_data = shotgun_model.get_sanitized_data(item, self.SG_ASSOCIATED_FIELD_ROLE)
+        # get the full sg data for this node (leafs only)
+        sg_data = shotgun_model.get_sg_data(item)     
+        
+        # {'name': 'sg_sequence', 'value': {'type': 'Sequence', 'id': 11, 'name': 'bunny_080'}}
+        field_value = field_data["value"]
+        
+        if isinstance(field_value, dict) and "name" in field_value and "type" in field_value:
+            # this is an intermediate node which is an entity type link
+            if field_value.get("type") in self._entity_icons:
+                # use sg icon!
+                item.setIcon(self._entity_icons[ field_value.get("type") ])
+                found_icon = True
+        
+        elif sg_data:
+            # this is a leaf node!  
+            if sg_data.get("type") in self._entity_icons:
+                # use sg icon!
+                item.setIcon(self._entity_icons[ sg_data.get("type") ])
+                found_icon = True
+        
+        # for all items where we didn't find the icon, fall back onto the default
+        if not found_icon:
+            item.setIcon(self._default_icon)
                 
     def _populate_item(self, item, sg_data):
         """

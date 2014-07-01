1ebeab41bb58d39b8859373e4c8a9abb3c932eb1
Manne Öhrström <manne@shotgunsoftware.com>
2014-07-01 11:23:32 +0100

    #25652 Fixed pixmap:scaled errors.

--- python/tk_multi_loader/utils.py
+++ python/tk_multi_loader/utils.py
@@ -19,24 +19,27 @@ def create_overlayed_user_publish_thumbnail(publish_pixmap, user_pixmap):
     base_image = QtGui.QPixmap(75, 75)
     base_image.fill(QtCore.Qt.transparent)
     
-    # scale down the thumb
-    thumb_scaled = publish_pixmap.scaled(75, 75, QtCore.Qt.KeepAspectRatioByExpanding, QtCore.Qt.SmoothTransformation)  
-
-    # now composite the thumbnail on top of the base image
-    # bottom align it to make it look nice
-    thumb_img = thumb_scaled.toImage()
-    brush = QtGui.QBrush(thumb_img)
-    
     painter = QtGui.QPainter(base_image)
     painter.setRenderHint(QtGui.QPainter.Antialiasing)
-        
-    painter.save() 
-    painter.setBrush(brush)
-    painter.setPen(QtGui.QPen(QtCore.Qt.NoPen))
-    painter.drawRect(0, 0, 74, 74) 
-    painter.restore()
     
-    if user_pixmap: 
+    # scale down the thumb
+    if not publish_pixmap.isNull():
+        thumb_scaled = publish_pixmap.scaled(75, 
+                                             75, 
+                                             QtCore.Qt.KeepAspectRatioByExpanding, 
+                                             QtCore.Qt.SmoothTransformation)  
+
+        # now composite the thumbnail on top of the base image
+        # bottom align it to make it look nice
+        thumb_img = thumb_scaled.toImage()
+        brush = QtGui.QBrush(thumb_img)
+        painter.save() 
+        painter.setBrush(brush)
+        painter.setPen(QtGui.QPen(QtCore.Qt.NoPen))
+        painter.drawRect(0, 0, 74, 74) 
+        painter.restore()
+    
+    if user_pixmap and not user_pixmap.isNull(): 
     
         # overlay the user picture on top of the thumbnail
         user_scaled = user_pixmap.scaled(30, 30, QtCore.Qt.KeepAspectRatioByExpanding, QtCore.Qt.SmoothTransformation)  
@@ -72,50 +75,52 @@ def create_overlayed_folder_thumbnail(path):
     # folder icon graphic
     MAX_THUMB_WIDTH = 460
     MAX_THUMB_HEIGHT = 280
-    
-    thumb = QtGui.QPixmap(path)
-    
-    # this is a thumbnail for a folder coming in!
-    # merge it with the current folder thumbnail image
-    res_base = QtGui.QPixmap(":/res/folder_512x400.png")
+
     # looks like there are some pyside related memory issues here relating to 
     # referencing a resource and then operating on it. Just to be sure, make 
-    # make a full copy of the resource before starting to manipulate.
+    # make a full copy of the resource before starting to manipulate.    
+    res_base = QtGui.QPixmap(":/res/folder_512x400.png")
     base_image = QtGui.QPixmap(res_base)
     
-    thumb_scaled = thumb.scaled(MAX_THUMB_WIDTH, 
-                                MAX_THUMB_HEIGHT, 
-                                QtCore.Qt.KeepAspectRatio, 
-                                QtCore.Qt.SmoothTransformation)  
+    # now attempt to load the image
+    # pixmap will be a null pixmap if load fails
+    thumb = QtGui.QPixmap(path)
+        
+    if not thumb.isNull():
+    
+        thumb_scaled = thumb.scaled(MAX_THUMB_WIDTH, 
+                                    MAX_THUMB_HEIGHT, 
+                                    QtCore.Qt.KeepAspectRatio, 
+                                    QtCore.Qt.SmoothTransformation)  
         
           
-    # now composite the thumbnail
-    thumb_img = thumb_scaled.toImage()
-    brush = QtGui.QBrush(thumb_img)
+        # now composite the thumbnail
+        thumb_img = thumb_scaled.toImage()
+        brush = QtGui.QBrush(thumb_img)
     
-    painter = QtGui.QPainter(base_image)
-    painter.setRenderHint(QtGui.QPainter.Antialiasing)
-    painter.setBrush(brush)
+        painter = QtGui.QPainter(base_image)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.setBrush(brush)
     
-    # figure out the offset height wise in order to center the thumb
-    height_difference = CANVAS_HEIGHT - thumb_scaled.height()
-    width_difference = CANVAS_WIDTH - thumb_scaled.width()
+        # figure out the offset height wise in order to center the thumb
+        height_difference = CANVAS_HEIGHT - thumb_scaled.height()
+        width_difference = CANVAS_WIDTH - thumb_scaled.width()
     
-    inlay_offset_w = (width_difference/2)+(CORNER_RADIUS/2)
-    # add a 30 px offset here to push the image off center to 
-    # fit nicely inside the folder icon
-    inlay_offset_h = (height_difference/2)+(CORNER_RADIUS/2)+30
+        inlay_offset_w = (width_difference/2)+(CORNER_RADIUS/2)
+        # add a 30 px offset here to push the image off center to 
+        # fit nicely inside the folder icon
+        inlay_offset_h = (height_difference/2)+(CORNER_RADIUS/2)+30
     
-    # note how we have to compensate for the corner radius
-    painter.translate(inlay_offset_w, inlay_offset_h)
-    painter.drawRoundedRect(0,  
-                            0, 
-                            thumb_scaled.width()-CORNER_RADIUS, 
-                            thumb_scaled.height()-CORNER_RADIUS, 
-                            CORNER_RADIUS, 
-                            CORNER_RADIUS)
-    
-    painter.end()
+        # note how we have to compensate for the corner radius
+        painter.translate(inlay_offset_w, inlay_offset_h)
+        painter.drawRoundedRect(0,  
+                                0, 
+                                thumb_scaled.width()-CORNER_RADIUS, 
+                                thumb_scaled.height()-CORNER_RADIUS, 
+                                CORNER_RADIUS, 
+                                CORNER_RADIUS)
+        
+        painter.end()
     
     return base_image
     
@@ -131,48 +136,52 @@ def create_overlayed_publish_thumbnail(path):
     CANVAS_WIDTH = 512
     CANVAS_HEIGHT = 400
     CORNER_RADIUS = 10
-    
-    thumb = QtGui.QPixmap(path)
-    
-    # scale it down to fit inside a frame of maximum 512x512
-    thumb_scaled = thumb.scaled(CANVAS_WIDTH, 
-                                CANVAS_HEIGHT, 
-                                QtCore.Qt.KeepAspectRatio, 
-                                QtCore.Qt.SmoothTransformation)  
 
     # get the 512 base image
     base_image = QtGui.QPixmap(CANVAS_WIDTH, CANVAS_HEIGHT)
     base_image.fill(QtCore.Qt.transparent)
+    
+    # now attempt to load the image
+    # pixmap will be a null pixmap if load fails    
+    thumb = QtGui.QPixmap(path)
+    
+    if not thumb.isNull():
+            
+        # scale it down to fit inside a frame of maximum 512x512
+        thumb_scaled = thumb.scaled(CANVAS_WIDTH, 
+                                    CANVAS_HEIGHT, 
+                                    QtCore.Qt.KeepAspectRatio, 
+                                    QtCore.Qt.SmoothTransformation)  
 
-    # now composite the thumbnail on top of the base image
-    # bottom align it to make it look nice
-    thumb_img = thumb_scaled.toImage()
-    brush = QtGui.QBrush(thumb_img)
-    
-    painter = QtGui.QPainter(base_image)
-    painter.setRenderHint(QtGui.QPainter.Antialiasing)
-    painter.setBrush(brush)
-    
-    # figure out the offset height wise in order to center the thumb
-    height_difference = CANVAS_HEIGHT - thumb_scaled.height()
-    width_difference = CANVAS_WIDTH - thumb_scaled.width()
-    
-    # center it with wise
-    inlay_offset_w = (width_difference/2)+(CORNER_RADIUS/2)
-    # bottom height wise
-    #inlay_offset_h = height_difference+CORNER_RADIUS
-    inlay_offset_h = (height_difference/2)+(CORNER_RADIUS/2)
-    
-    # note how we have to compensate for the corner radius
-    painter.translate(inlay_offset_w, inlay_offset_h)
-    painter.drawRoundedRect(0,  
-                            0, 
-                            thumb_scaled.width()-CORNER_RADIUS, 
-                            thumb_scaled.height()-CORNER_RADIUS, 
-                            CORNER_RADIUS, 
-                            CORNER_RADIUS)
-    
-    painter.end()
+        # now composite the thumbnail on top of the base image
+        # bottom align it to make it look nice
+        thumb_img = thumb_scaled.toImage()
+        brush = QtGui.QBrush(thumb_img)
+        
+        painter = QtGui.QPainter(base_image)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.setBrush(brush)
+        
+        # figure out the offset height wise in order to center the thumb
+        height_difference = CANVAS_HEIGHT - thumb_scaled.height()
+        width_difference = CANVAS_WIDTH - thumb_scaled.width()
+        
+        # center it with wise
+        inlay_offset_w = (width_difference/2)+(CORNER_RADIUS/2)
+        # bottom height wise
+        #inlay_offset_h = height_difference+CORNER_RADIUS
+        inlay_offset_h = (height_difference/2)+(CORNER_RADIUS/2)
+        
+        # note how we have to compensate for the corner radius
+        painter.translate(inlay_offset_w, inlay_offset_h)
+        painter.drawRoundedRect(0,  
+                                0, 
+                                thumb_scaled.width()-CORNER_RADIUS, 
+                                thumb_scaled.height()-CORNER_RADIUS, 
+                                CORNER_RADIUS, 
+                                CORNER_RADIUS)
+        
+        painter.end()
     
     return base_image
     

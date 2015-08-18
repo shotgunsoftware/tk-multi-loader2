7782e4efa51921d52b5695dbc405803a6157d0af
Adam Wierzchowski <awierzchowski@rodeofx.com>
2015-08-18 16:42:18 -0400

    python/tk_multi_loader/dialog.py: change variable in if condition

--- python/tk_multi_loader/dialog.py
+++ python/tk_multi_loader/dialog.py
@@ -684,7 +684,7 @@ class AppDialog(QtGui.QWidget):
             # no suitable item found. Use the first tab
             found_preset = self.ui.entity_preset_tabs.tabText(0)
 
-        if found_model and item:
+        if found_model and found_item:
             found_model.reSelector['entity'] = found_item.get_sg_data()
             found_model.reSelector['found_preset'] = found_preset
             found_model.reSelector['sel_func'] = self._select_item_in_entity_tree

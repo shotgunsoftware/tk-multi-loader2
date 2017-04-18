cef4bf891ee3eb0a2615605c116b426623ebcfe5
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-04-18 14:27:52 -0400

    better doc

--- python/tk_multi_loader/dialog.py
+++ python/tk_multi_loader/dialog.py
@@ -1354,9 +1354,9 @@ class AppDialog(QtGui.QWidget):
 
     def _hierarchy_refreshed(self):
         """
-        Signal triggered when someone changes the selection in a treeview.
+        Signal triggered when the hierarchy model has been refreshed. This allows to show all the
+        folder items in the right-hand side for the current selection.
         """
-
         selected_item = self._get_selected_entity()
 
         # tell publish UI to update itself

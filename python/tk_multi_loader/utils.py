2838a5a79e133838387690880f4a9c09cd150ac1
Philip Scadding <pscadding@users.noreply.github.com>
2018-04-13 14:47:40 +0100

    SG-5399 Added support for complex filters (#67)
    Added support for complex filters on tabs.
    You can now use filter operators, like the following
    
    ```
    - caption: Tasks
        entity_type: Task
        filters:
          - [project, is, "{context.project}"]
          - filter_operator: "any"
            filters:
              - [entity, type_is, Asset]
              - [entity, type_is, Shot]
    ```
    
    Closes #67

--- python/tk_multi_loader/utils.py
+++ python/tk_multi_loader/utils.py
@@ -8,6 +8,7 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+import sgtk
 from sgtk.platform.qt import QtCore, QtGui
 
 
@@ -276,3 +277,45 @@ def filter_publishes(app, sg_data_list):
         sg_data_list = []
 
     return sg_data_list
+
+
+def resolve_filters(filters):
+    """
+    When passed a list of filters, it will resolve strings found in the filters using the context.
+    For example: '{context.user}' could get resolved to {'type': 'HumanUser', 'id': 86, 'name': 'Philip Scadding'}
+
+    :param filters: A list of filters that has usually be defined by the user or by default in the environment yml
+    config or the app's info.yml. Supports complex filters as well. Filters should be passed in the following format:
+    [[task_assignees, is, '{context.user}'],[sg_status_list, not_in, [fin,omt]]]
+
+    :return: A List of filters for use with the shotgun api
+    """
+    app = sgtk.platform.current_bundle()
+
+    resolved_filters = []
+    for filter in filters:
+        if type(filter) is dict:
+            resolved_filter = {
+                "filter_operator": filter["filter_operator"],
+                "filters": resolve_filters(filter["filters"])}
+        else:
+            resolved_filter = []
+            for field in filter:
+                if field == "{context.entity}":
+                    field = app.context.entity
+                elif field == "{context.step}":
+                    field = app.context.step
+                elif field == "{context.project}":
+                    field = app.context.project
+                elif field == "{context.project.id}":
+                    if app.context.project:
+                        field = app.context.project.get("id")
+                    else:
+                        field = None
+                elif field == "{context.task}":
+                    field = app.context.task
+                elif field == "{context.user}":
+                    field = app.context.user
+                resolved_filter.append(field)
+        resolved_filters.append(resolved_filter)
+    return resolved_filters

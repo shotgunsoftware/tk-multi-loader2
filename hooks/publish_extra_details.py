fd23a80cae0ac8d9a529a423ff3fa5e7a623cf71
Barbara Laigneau <barbara@dark-shot.com>
2019-10-18 17:04:21 +0200

    use HookBaseClass instead of Hook class; rename parameter; improve comments; return a dictionary instead of a tuple

--- hooks/publish_extra_details.py
+++ hooks/publish_extra_details.py
@@ -8,17 +8,20 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-from sgtk import Hook
+import sgtk
+
+HookBaseClass = sgtk.get_hook_baseclass()
 
 
-class PublishExtraDetails(Hook):
+class PublishExtraDetails(HookBaseClass):
     """Hook that can be used to add extra fields in the details pane."""
 
-    def execute(self, sg_item, **kwargs):
+    def execute(self, entity):
         """
         Main hook entry point.
 
-        :param sg_item: published item
-        :return List: Tuple of tuples with the form (Label, Value)
+        :param entity: The entity dictionary associated with the published item to be displayed in the loader app.
+        :return: A dictionary in the form of {label: value}
+        :rtype: dictionary
         """
-        return ()
+        return {}

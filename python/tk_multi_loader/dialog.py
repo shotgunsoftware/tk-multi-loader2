30bd4791fa787599350c7d945f0bf4321a2accc1
Barbara Laigneau <barbara@dark-shot.com>
2019-10-18 17:07:40 +0200

    fix hook parameters to avoid using internal method

--- python/tk_multi_loader/dialog.py
+++ python/tk_multi_loader/dialog.py
@@ -738,10 +738,9 @@ class AppDialog(QtGui.QWidget):
                 # if there are extra fields related to published files to be displayed
                 extra_details = sgtk.platform.current_bundle().execute_hook(
                     "publish_extra_details_hook",
-                    sg_item=sg_item,
-                    get_long_name_method=self._status_model.get_long_name
+                    entity=sg_item
                 )
-                for label, value in extra_details:
+                for label, value in extra_details.iteritems():
                     msg += __make_table_row(label, value)
 
                 self.ui.details_header.setText("<table>%s</table>" % msg)

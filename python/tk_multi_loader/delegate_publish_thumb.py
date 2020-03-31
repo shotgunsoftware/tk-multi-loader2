73c60917095c7ecc57bc186b64bf46aa2aee4f54
Ariel Calzada <ariel.calzada.solano@autodesk.com>
2020-03-31 08:05:42 -0500

    Fixed some linter issues

--- python/tk_multi_loader/delegate_publish_thumb.py
+++ python/tk_multi_loader/delegate_publish_thumb.py
@@ -224,7 +224,9 @@ class SgPublishThumbDelegate(PublishDelegate):
             if entity_link is None:
                 details_text = "Unlinked"
             else:
-                entity_link_type = shotgun_globals.get_type_display_name(entity_link["type"])
+                entity_link_type = shotgun_globals.get_type_display_name(
+                    entity_link["type"]
+                )
                 details_text = "%s %s" % (entity_link_type, entity_link["name"])
 
         else:

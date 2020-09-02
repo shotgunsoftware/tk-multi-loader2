40f12ec52851931bfa8eac117bcef2c6ce3c5944
Ehsan Shokrgozar <ehsan.shokrgozar@autodesk.com>
2020-09-02 08:02:43 -0700

    Future proofing the hooks

--- hooks/tk-nuke_actions.py
+++ hooks/tk-nuke_actions.py
@@ -17,6 +17,7 @@ import glob
 import sys
 
 import sgtk
+from tank_vendor import six
 
 HookBaseClass = sgtk.get_hook_baseclass()
 
@@ -221,7 +222,7 @@ class NukeActions(HookBaseClass):
         import nuke
 
         # must use unicode otherwise path won't be found
-        if not os.path.exists(path.decode("utf-8")):
+        if not os.path.exists(six.ensure_str(path)):
             raise Exception("File not found on disk - '%s'" % path)
 
         nuke.nodePaste(path)

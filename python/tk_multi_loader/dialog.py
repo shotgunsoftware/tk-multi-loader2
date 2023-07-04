441d0c60b08827f500845c3186e76e5fc6ddab4d
Norberto Moreno Velez <norbermv.dev@gmail.com>
2023-07-04 10:27:47 -0500

    Remove unused import.

--- python/tk_multi_loader/dialog.py
+++ python/tk_multi_loader/dialog.py
@@ -8,7 +8,7 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import sys
+
 import sgtk
 from sgtk import TankError
 from sgtk.platform.qt import QtCore, QtGui

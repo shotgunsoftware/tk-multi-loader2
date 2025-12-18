1f139f41ffc6c5329074d15bd9d8799d403f2a1c
Eduardo Chauca <eduardo.chauca@autodesk.com>
2025-12-18 13:37:00 -0500

    improvements

--- python/tk_multi_loader/loader_action_manager.py
+++ python/tk_multi_loader/loader_action_manager.py
@@ -8,10 +8,11 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import sgtk
+import functools
 import os
 import sys
-from functools import partial
+
+import sgtk
 from sgtk.platform.qt import QtCore, QtGui
 from sgtk.util import login
 
@@ -100,8 +101,9 @@ class LoaderActionManager(ActionManager):
             ]
 
             # Bind all the action params to a single invocation of the _execute_hook.
-            handler = partial(self._execute_hook, qt_action=a, actions=actions)
-            a.triggered[()].connect(handler)
+            a.triggered[()].connect(
+                functools.partial(self._execute_hook, qt_action=a, actions=actions)
+            )
 
             a.setData(actions)
             qt_actions.append(a)

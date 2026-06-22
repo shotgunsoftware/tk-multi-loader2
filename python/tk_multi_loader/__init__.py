38e0e20ea592ca457c3e4c5936b7181635f2cc60
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2026-06-22 09:05:59 -0500

    Import flowam module just to be safe

--- python/tk_multi_loader/__init__.py
+++ python/tk_multi_loader/__init__.py
@@ -8,6 +8,7 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+from . import flowam  # noqa: F401
 from .api import LoaderManager  # noqa: F401
 from .open_publish_form import open_publish_browser  # noqa: F401
 
